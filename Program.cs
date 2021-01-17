c53cd4af125faa615f974ac280e5d25d21657931
joemacko <joemacko58@gmail.com>
2021-01-17 13:28:53 -0500

    Adding GoldBadgeEndTest to GitHub

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoldBadgeEndTest
+{
+    class Program
+    {
+        //public static int FindMaxSum(List<int> list)
+        //{
+        //    if (list != null)
+        //    {
+        //        List<int> newList = new List<int>();
+        //        var maxValue = newList.Max(num => num);
+        //        var secondHighest = newList.OrderByDescending(num => num).Skip(1).First();
+        //    }
+        //    else
+        //    {
+        //        null;
+        //    }
+        //}
+        //static void Main(string[] args)
+        //{
+        //    List<int> list = new List<int> { 5, 9, 7, 11 };
+        //    Console.WriteLine(FindMaxSum(list));
+        //}
+    }
+}
